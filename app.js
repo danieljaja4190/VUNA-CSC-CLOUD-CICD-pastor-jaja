c53b61d648ac09fd642413b33c12e286f61f974e
danieljaja4190 <140502244+danieljaja4190@users.noreply.github.com>
2024-05-29 19:30:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Assuming you have an index.html file in the 'public' directory
+});
+
+app.get('/about', (req, res) => {
+    res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS.  JAJA PASTOR ----------------- VUG/CSC/21/5879');
+});
+
+app.listen(port, () => {
+    console.log(Server is running on http://localhost:${port});
+});
